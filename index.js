0174be0581646ed44ca42f00ae541d79ffce9fcc
ahad30 <mohiminulislamahad@gmail.com>
2024-04-26 23:32:15 +0600

    post api created successfully

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxclpw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,12 +25,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const artCraftCollection = client.db('artCraftDB').collection('artCraft');
+
+    app.post('/addArtCraftItem', async (req, res) => {
+      const newArtCraft = req.body;
+      console.log(newArtCraft);
+      const result = await artCraftCollection.insertOne(newArtCraft);
+      res.send(result);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -39,7 +60,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Art and Craft server is running')
+    res.send('Art and Craft server')
 })
 
 app.listen(port, () => {
